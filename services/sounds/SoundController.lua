c283d20c26c50828e3b63fd29766804e2c92d76b
tauerlund <tauerlund@gmail.com>
2025-06-18 21:44:09 +0200

    Initial sound effect support

--- services/sounds/SoundController.lua
+++ services/sounds/SoundController.lua
@@ -1,15 +1,37 @@
-local events = require("tauer.modern-lockpicking.shared.enums.events")
+local EVENTS = require("tauer.modern-lockpicking.shared.enums.events")
+local CONSTANTS = require("tauer.modern-lockpicking.services.sounds.enums.constants")
 
 ---@class SoundController : IInitializedService
 local this = {}
 
 ---@public
 function this.Initialize()
-	event.register(events.lockpickingEnded, this.onLockUnlocked)
+	this.registerEvents()
 	return true
 end
 
 ---@private
-function this.onLockUnlocked() end
+---@param _ lockpickingEndedEventData
+function this.onLockpickingEnd(_)
+	tes3.playSound({
+		reference = tes3.player,
+		soundPath = string.format("%s/%s.wav", CONSTANTS.basePath, table.choice(CONSTANTS.sounds.unlock)),
+	})
+end
+
+---@private
+---@param _ pickChangeEventData
+function this.onPickChange(_)
+	tes3.playSound({
+		reference = tes3.player,
+		soundPath = string.format("%s/%s.wav", CONSTANTS.basePath, table.choice(CONSTANTS.sounds.lockpicks)),
+	})
+end
+
+---@private
+function this.registerEvents()
+	event.register(EVENTS.lockpickingEnd, this.onLockpickingEnd)
+	event.register(EVENTS.pickChange, this.onPickChange)
+end
 
 return this
---
+++ services/sounds/enums/constants.lua
@@ -0,0 +1,20 @@
+---@enum SOUND_CONSTANTS
+local enum = {
+    basePath = "tauer/modern-lockpicking",
+    sounds = {
+        lockpicks = {
+            "lockpick-1",
+            "lockpick-2",
+            "lockpick-3",
+            "lockpick-4",
+        },
+        unlock = {
+            "unlock-1",
+            "unlock-2",
+            "unlock-3",
+            "unlock-4",
+        },
+    }
+}
+
+return enum
