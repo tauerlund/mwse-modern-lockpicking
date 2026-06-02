b5c7bf9edc1cf6bc981cbcf1c7b7b73a648c4472
tauerlund <tauerlund@gmail.com>
2026-06-02 23:40:16 +0200

    Add lock tilt gamefeel

--- services/locks/lockAnimator.lua
+++ services/locks/lockAnimator.lua
@@ -1,6 +1,6 @@
 --- ENUMS
 local EVENTS = require("tauer.modern-lockpicking.services.events.enums.EVENTS")
-local CONSTANTS = require("tauer.modern-lockpicking.services.locks.enums.CONSTANTS")
+local CONSTANTS = require("tauer.modern-lockpicking.services.locks.enums.constants")
 ---
 
 ---@class lockAnimator : initializedService
@@ -10,6 +10,48 @@ local this = {}
 ---@type timerManager
 this.timerManager = nil
 
+---@private
+---@type lock|nil
+this.lock = nil
+
+---@private
+---@type tes3matrix33|nil
+this.initialRotation = nil
+
+---@private
+---@type number
+this.currentRotX = 0
+
+---@private
+---@type number
+this.currentRotY = 0
+
+---@private
+---@type number
+this.startCursorX = 0
+
+---@private
+---@type number
+this.startCursorY = 0
+
+---@private
+---@type boolean
+this.paused = false
+
+---@private
+this.mouseConstants = {
+	amplitude = 0.15,
+	lerpSpeed = 5,
+}
+
+---@private
+---@type tes3matrix33
+this.rotationBufferX = tes3matrix33.new()
+
+---@private
+---@type tes3matrix33
+this.rotationBufferY = tes3matrix33.new()
+
 ---@public
 ---@param services serviceCollection
 ---@return boolean,string|nil
@@ -17,7 +59,21 @@ function this.initialize(services)
 	this.timerManager = services.timerManager
 
 	event.register(EVENTS.lockpickingStart, this.onLockpickingStart)
+	event.register(EVENTS.lockpickingStarted, this.onLockpickingStarted)
+	event.register(EVENTS.lockpickingEnded, this.onLockpickingEnded)
+	event.register(EVENTS.optionsMenuOpened, this.onOptionsMenuOpened)
+	event.register(EVENTS.optionsMenuClosed, this.onOptionsMenuClosed)
 	return true, nil
+end
+
+---@private
+function this.onOptionsMenuOpened()
+	this.paused = true
+end
+
+---@private
+function this.onOptionsMenuClosed()
+	this.paused = false
 end
 
 ---@private
@@ -36,6 +92,72 @@ function this.onLockpickingStart(e)
 			targetTranslation = this.getTargetTranslation(lock),
 		},
 	})
+end
+
+---@private
+---@param e lockpickingStartedEventData
+function this.onLockpickingStarted(e)
+	this.lock = e.session.lock
+	this.initialRotation = e.session.lock.mesh.rotation:copy()
+	this.currentRotX = 0
+	this.currentRotY = 0
+	local cursor = tes3.getCursorPosition()
+	this.startCursorX = cursor.x
+	this.startCursorY = cursor.y
+	this.enable()
+end
+
+---@private
+---@param _ lockpickingEndedEventData
+function this.onLockpickingEnded(_)
+	this.lock = nil
+	this.initialRotation = nil
+	this.currentRotX = 0
+	this.currentRotY = 0
+	this.disable()
+end
+
+---@private
+function this.enable()
+	if not event.isRegistered(tes3.event.enterFrame, this.onEnterFrame) then
+		event.register(tes3.event.enterFrame, this.onEnterFrame)
+	end
+end
+
+---@private
+function this.disable()
+	if event.isRegistered(tes3.event.enterFrame, this.onEnterFrame) then
+		event.unregister(tes3.event.enterFrame, this.onEnterFrame)
+	end
+end
+
+---@private
+---@param e enterFrameEventData
+function this.onEnterFrame(e)
+	if this.paused or not this.lock then
+		return
+	end
+
+	local constants = this.mouseConstants
+
+	local cursor = tes3.getCursorPosition()
+	local viewportWidth, viewportHeight = tes3.getViewportSize()
+
+	local currentRotX = this.currentRotX
+	local currentRotY = this.currentRotY
+
+	local targetRotX = ((this.startCursorY - cursor.y) / viewportHeight) * constants.amplitude
+	local targetRotY = ((cursor.x - this.startCursorX) / viewportWidth) * constants.amplitude
+
+	local t = math.min(1, constants.lerpSpeed * e.delta)
+	this.currentRotX = currentRotX + (targetRotX - currentRotX) * t
+	this.currentRotY = currentRotY + (targetRotY - currentRotY) * t
+
+	this.rotationBufferX:toRotationX(this.currentRotX)
+	this.rotationBufferY:toRotationY(this.currentRotY)
+
+	this.lock.mesh.rotation = this.rotationBufferX * this.rotationBufferY * this.initialRotation
+	this.lock.mesh:update()
 end
 
 ---@private
