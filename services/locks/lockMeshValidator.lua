3b9834e3e4e3352e6f8032e81ad50645b1575fd8
tauerlund <tauerlund@gmail.com>
2026-06-08 00:50:46 +0200

    Fix lockMeshValidator

--- services/locks/lockMeshValidator.lua
+++ services/locks/lockMeshValidator.lua
@@ -17,9 +17,11 @@ end
 ---@param mesh niNode
 ---@return boolean, reason
 function this.validate(mesh)
-    for _, objectName in ipairs(this.enums.objectNames) do
-        if not mesh:getObjectByName(objectName) then
-            return false, string.format("object '%s' is missing", mesh.name, objectName)
+    local names = this.enums.objectNames
+
+    for _, name in ipairs({ names.cylinderHelper, names.knifeHelper, names.pickHelper }) do
+        if not mesh:getObjectByName(name) then
+            return false, string.format("object '%s' is missing", mesh.name, name)
         end
     end
     return true, nil
