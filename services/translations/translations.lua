3c56ab0fa89c501e04b030f3fc2bafe823fd938a
tauerlund <tauerlund@gmail.com>
2026-06-10 22:12:36 +0200

    Cache mwse.loadTranslations

--- services/translations/translations.lua
+++ services/translations/translations.lua
@@ -1,10 +1,18 @@
 ---@class translations
 local this = {}
 
+---@private
+---@type fun(key: string, data?: any):string
+this.load = nil
+
 ---@public
 ---@param key translationKeys
 function this.get(key)
-	return mwse.loadTranslations("tauer.modern-lockpicking")(key)
+	if not this.load then
+		this.load = mwse.loadTranslations("tauer.modern-lockpicking")
+	end
+
+	return this.load(key)
 end
 
 return this
