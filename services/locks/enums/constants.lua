7a3b93a7d688b5ec17c9d0a2bb292c8a7bd6dfa8
tauerlund <tauerlund@gmail.com>
2026-06-18 21:08:17 +0200

    Fix mesh name

--- services/locks/enums/constants.lua
+++ services/locks/enums/constants.lua
@@ -3,7 +3,7 @@ local enum = {
 	targetDistance = 80,
 	referenceVerticalTan = math.tan(math.rad(75) * 0.5) * (9 / 16),
 	paths = {
-		defaultLockMesh = "tauer\\lock_new.nif",
+		defaultLockMesh = "tauer\\lock.nif",
 	},
 	rootName = "ModernLockpicking:Root",
 	cameraRootZBufferName = "ModernLockpicking:NoDepth",
